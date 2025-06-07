ea10897cbe4c7fc9fe8f5fb1d311ea879711836e
felipe w <callofnoob712@gmail.com>
2025-06-07 15:16:16 -0300

    Feat: Criada conexão com o banco utilizando sequelize

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,14 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+dotenv.config();
 
-export default new Sequelize();
-// export default new Sequelize(
-//   process.env.POSTGRES_DB,
-//   process.env.POSTGRES_USERNAME,
-//   process.env.POSTGRES_PASSWORD,
-//   {
-//     host: process.env.POSTGRES_HOST,
-//     dialect: 'postgres',
-//     schema: process.env.POSTGRES_SCHEMA,
-//     port: process.env.POSTGRES_PORT,
-//   },
-// );
+export default new Sequelize(
+  process.env.POSTGRES_DB,
+  process.env.POSTGRES_USERNAME,
+  process.env.POSTGRES_PASSWORD,
+  {
+    host: process.env.POSTGRES_HOST,
+    dialect: 'postgres',
+    port: process.env.POSTGRES_PORT,
+  },
+);
