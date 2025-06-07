78d15c511ebada9add9b14845dd91d296a2288e1
felipe w <callofnoob712@gmail.com>
2025-06-07 17:28:43 -0300

    Fix: corrigido erro do cors para requisições entre URL's diferentes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import express from 'express';
 import routes from './routes/index.js';
+import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const porta = 3333;
+
+app.use(cors());  
 
 app.use(express.json());
 
